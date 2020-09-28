f11abb25434d0a7e7b561626a0b31a1979ac63a3
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-09-28 18:57:47 +0400

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Form @submitForm="onFormSubmit"/>
+        <TotalBalance :total="totalBalance"/>
+        <BudgetList :list="list" @deleteItem="onDeleteItem"/>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import BudgetList from "./components/BudgetList";
+    import TotalBalance from "./components/TotalBalance";
+    import Form from "./components/Form";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            BudgetList,
+            TotalBalance,
+            Form
+        },
+        data() {
+            return {
+                list: {
+                    1: {
+                        type: 'INCOME',
+                        value: 100,
+                        comment: 'some comments',
+                        id: 1,
+                    },
+                    2: {
+                        type: 'OUTCOME',
+                        value: -50,
+                        comment: 'some outcome comments',
+                        id: 2,
+                    },
+                },
+            }
+
+        },
+        computed: {
+            totalBalance() {
+                return Object.values(this.list).reduce((acc, item) => acc + item.value, 0);
+            }
+        },
+        methods: {
+            onDeleteItem(id) {
+                this.$delete(this.list, id)
+            },
+            onFormSubmit(data) {
+                console.log(data);
+            }
+
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/BudgetList.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="budget-list-wrap">
+        <ElCard :header="header">
+            <template v-if="!isEmpty">
+                <div class="list-item" v-for="(item, prop ) in list" :key="prop">
+                    <span class="budget-comment">{{item.comment}}</span>
+                    <span class="budget-value">{{item.value}}</span>
+                    <ElButton type="danger" size="mini" @click="deleteItem(item.id)">Delete</ElButton>
+                </div>
+            </template>
+            <ElAlert v-else :title="emptyList" closable="false" />
+        </ElCard>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BudgetList",
+        props: {
+            list: {
+                type: Object,
+                default: () => ({}),
+            }
+        },
+        data: () => ({
+            header: "Budget List",
+            emptyList: "Empty List"
+        }),
+        computed: {
+            isEmpty() {
+                return !Object.keys(this.list).length
+
+            }
+        },
+        methods: {
+            deleteItem(id){
+               this.$emit("deleteItem", id);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .budget-list-wrap {
+        margin: auto;
+        max-width: 500px;
+    }
+
+    .list-item {
+        display: flex;
+        align-items: center;
+        padding: 10px 15px;
+    }
+
+    .budget-value {
+        margin-left: auto;
+        margin-right: 20px;
+        font-weight: bold;
+    }
+</style>
